c2095113fe4fc3a394babf161dc6988db01149cb
orientcode <orientcode@gmail.com>
2019-05-30 10:25:18 -0300

    arquivos

--- girassol.ino
+++ girassol.ino
@@ -3,12 +3,12 @@
 Servo servoY;
 Servo servoX;
 
-#define quadrante1 A1
-#define quadrante2 A2
-#define quadrante3 A3
-#define quadrante4 A4
+#define quadrante1 A4
+#define quadrante2 A3
+#define quadrante3 A2
+#define quadrante4 A1
 
-#define VALOR 800
+#define VALOR 200
 
 int anguloX = 100;
 int anguloY = 180;
@@ -32,7 +32,44 @@ void loop() {
   int valorQuadrante3 = analogRead(quadrante3);
   int valorQuadrante4 = analogRead(quadrante4);
 
+  Serial.print("Quadrante 01: ");
   Serial.println(valorQuadrante1);
+  Serial.print("Quadrante 02: ");
+  Serial.println(valorQuadrante2);
+  Serial.print("Quadrante 03: ");
+  Serial.println(valorQuadrante3);
+  Serial.print("Quadrante 04: ");
+  Serial.println(valorQuadrante4);
+  Serial.println("--------------");
+  Serial.print("Angulo X: ");
+  Serial.println(anguloX);
+  Serial.print("Angulo Y: ");
+  Serial.println(anguloY);
+  Serial.println("---------------------------------");
   
-  delay(100);
+  if (anguloX >= 176) anguloX = 176;
+  if (anguloX <= 10) anguloX = 10;
+  if (anguloY >= 176) anguloY = 176;
+  if (anguloY <= 10) anguloY = 10;
+  
+  if ((valorQuadrante1 + valorQuadrante2 + valorQuadrante3 + valorQuadrante4) / 4 > VALOR) {
+    if ((valorQuadrante1 + valorQuadrante2) > (valorQuadrante3 + valorQuadrante4)) {
+      anguloX -= 2;
+    } else {
+      anguloX += 2; 
+    } 
+    if ((valorQuadrante1 + valorQuadrante4) > (valorQuadrante2 + valorQuadrante3)) {
+      anguloY -= 2;
+    } else {
+      anguloY += 2;
+    }
+  }
+  else {
+    anguloX = 100;
+    anguloY = 176;
+    delay(100);
+  }
+  servoX.write(anguloX);
+  servoY.write(anguloY);
+  delay(20);
 }
